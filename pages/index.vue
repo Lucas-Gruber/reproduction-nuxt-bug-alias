c05604f9752085dc47d1988cc667bd2dbfd15183
lucas-gruber <blaumc@icloud.com>
2024-04-01 01:48:24 +0200

    initial

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Index</h1>
+    <NuxtLink to="/test1">To alias-test</NuxtLink>
+  </div>
+</template>
---
+++ pages/test1/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { path } = useRoute()
+
+definePageMeta({
+  alias: '/test2',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Alias-test-page path: {{ path }}</h1>
+    <NuxtLink to="/test1">test1</NuxtLink>
+    <br />
+    <NuxtLink to="/test2">test2</NuxtLink>
+  </div>
+</template>
